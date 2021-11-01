da1cc640468463f12dc0e98e349db475d371487d
kendric23 <78301507+kendric23@users.noreply.github.com>
2021-11-01 22:34:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+  
+var engine, world;
+
+ 
+
+function setup() {
+	createCanvas(1600, 700);
+	
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	paper =new paper (100,600,10);
+	
+	ground=new ground(width/2,670,width,20);
+	
+
+	leftSide= new dustbin(550,620,20,100);
+	bottom= new dustbin(610,660,100,20);
+	rightSide=new dustbin(670,620,20,100);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  Engine.update(engine);
+
+  paper.display();
+ 
+
+  ground.display();
+  
+
+  leftSide.display();
+  bottom.display();
+  rightSide.display();
+
+  drawSprites();
+
+}
+
+function keyPressed(){
+	if(keyCode === 38){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:15, y: -15})
+	}
+}
+
